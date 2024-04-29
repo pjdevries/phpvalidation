fe6b921c5c5cef02396a43ad6de875d5444454f0
Pieter-Jan de Vries <pieter@obix.nl>
2024-04-29 09:56:40 +0200

    Adds tests.

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -1,13 +1,17 @@
 <?php
+
 namespace Test\Obix\Validator;
 
 use Obix\Validator\Rule\Alphabetic;
 use Obix\Validator\Rule\Choice;
 use Obix\Validator\Rule\Custom;
 use Obix\Validator\Rule\Email;
+use Obix\Validator\Rule\Exists;
 use Obix\Validator\Rule\Integer;
+use Obix\Validator\Rule\Not;
 use Obix\Validator\Rule\NotNull;
 use Obix\Validator\Rule\Numeric;
+use Obix\Validator\Rule\RuleList;
 use Obix\Validator\Rule\StringLength;
 use Obix\Validator\Rule\Url;
 use Obix\Validator\Validator;
@@ -16,9 +20,62 @@ use Test\Obix\Validator\Mock\Request;
 
 class ValidatorTest extends TestCase
 {
-    private function getValidator(): Validator
+    public function testNotNull(): void
     {
-        return new Validator([
+        $this->assertTrue(
+            ($validator = new Validator([
+                'not null' => new NotNull(),
+                'not not null' => new Not(new NotNull())
+            ]))->validateRequest(
+                Request::create([
+                    'not null' => 'foo',
+                    'not not null' => ''
+                ])
+            )
+        );
+
+        $this->assertFalse(
+            ($validator = new Validator([
+                'not null' => new NotNull(),
+                'not not null' => new Not(new NotNull()),
+                'null not exist' => new NotNull()
+            ]))->validateRequest(
+                Request::create([
+                    'not null' => '',
+                    'not not null' => 'foo'
+                ])
+            )
+        );
+    }
+
+    public function testExists(): void
+    {
+        $this->assertTrue(
+            ($validator = new Validator([
+                'exists' => new Exists(),
+                'not exists' => new Not(new Exists())
+            ]))->validateRequest(
+                Request::create([
+                    'exists' => 'foo'
+                ])
+            )
+        );
+
+        $this->assertFalse(
+            ($validator = new Validator([
+                'exists' => new Exists(),
+                'not exists' => new Not(new Exists())
+            ]))->validateRequest(
+                Request::create([
+                    'not exists' => 'foo'
+                ])
+            )
+        );
+    }
+
+    private function getRules(): array
+    {
+        return [
             'NotNull' => new NotNull(),
             'Email' => new Email(),
             'Alphabetic' => new Alphabetic(),
@@ -32,13 +89,23 @@ class ValidatorTest extends TestCase
             'Custom is bool' => new Custom(function ($value) {
                 return is_bool($value);
             })
-        ]);
+        ];
+    }
+
+    private function getCollectionRules(): array
+    {
+        return [
+            'Email or Null' => (new RuleList(new Not(new NotNull()), new Email()))
+                ->setBoolOp(RuleList::OR),
+            'Email or Url' => new RuleList(
+                new NotNull(), (new RuleList(new Url(), new Email()))
+                ->setBoolOp(RuleList::OR)
+            ),
+        ];
     }
 
     public function testSuccess()
     {
-        $validator = $this->getValidator();
-
         $tests = [
             'NotNull' => 'Foo',
             'Email' => 'pieter@obix.nl',
@@ -53,13 +120,25 @@ class ValidatorTest extends TestCase
             'Custom is bool' => true
         ];
 
-        $this->assertTrue($validator->validateRequest(Request::create($tests)));
+        $this->assertTrue((new Validator($this->getRules()))->validateRequest(Request::create($tests)));
+
+        $collectionRules = $this->getCollectionRules();
+        $testRules = [
+            'Email (or Null)' => $collectionRules['Email or Null'],
+            'Null (or Email)' => $collectionRules['Email or Null'],
+            'Email (or Url)' => $collectionRules['Email or Url'],
+            'Url (or Email)' => $collectionRules['Email or Url'],
+        ];
+        $collectionTests = [
+            'Email (or Url)' => 'pieter@obix.nl',
+            'Url (or Email)' => 'https://www.obix.nl',
+        ];
+
+        $this->assertTrue((new Validator($testRules))->validateRequest(Request::create($collectionTests)));
     }
 
     public function testFailure()
     {
-        $validator = $this->getValidator();
-
         $tests = [
             'NotNull' => null,
             'Email' => 'pieter_at_obix.nl',
@@ -74,8 +153,26 @@ class ValidatorTest extends TestCase
             'Custom is bool' => 0
         ];
 
+        $validator = new Validator($this->getRules());
         $this->assertFalse($validator->validateRequest(Request::create($tests)));
+        $errors = $validator->getErrors();
 
+        $collectionRules = $this->getCollectionRules();
+        $testRules = [
+            'Email (or Null)' => $collectionRules['Email or Null'],
+            'Null (or Email)' => $collectionRules['Email or Null'],
+            'Email (or Url)' => $collectionRules['Email or Url'],
+            'Url (or Email)' => $collectionRules['Email or Url'],
+        ];
+        $collectionTests = [
+            'Email (or Null)' => 'bar',
+            'Null (or Email)' => '',
+            'Email (or Url)' => '#pieter@obix.nl',
+            'Url (or Email)' => '#https://www.obix.nl',
+        ];
+
+        $validator = new Validator($testRules);
+        $this->assertFalse($validator->validateRequest(Request::create($collectionTests)));
         $errors = $validator->getErrors();
     }
 }
